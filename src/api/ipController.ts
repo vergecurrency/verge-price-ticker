a06b50c0cbbd7165664d4a44710e400e07ef0ca5
Swen van Zanten <info@hdsign.nl>
2019-02-26 22:43:14 +0100

    Use the users IP instead of the servers IP :D

--- src/api/ipController.ts
+++ src/api/ipController.ts
@@ -4,11 +4,13 @@ import { sendError, sendJSON } from "./defaultController";
 
 export const getIpDetails = async (req: Request, res: Response) => {
   try {
-      fetch("http://api.ipstack.com/check?access_key=a9e03264eab585d224212a5edcac8fcf&format=1")
-        .then(response => response.json())
-        .then(json => {
-            sendJSON(res, json);
-        });
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+    fetch(`http://api.ipstack.com/${ip}?access_key=a9e03264eab585d224212a5edcac8fcf&format=1`)
+      .then(response => response.json())
+      .then(json => {
+        sendJSON(res, json);
+      });
   } catch (e) {
     sendError(res, "Couldn't fetch IP data.");
   }
